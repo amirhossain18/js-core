ccc2bbb3fcca2d21952a28763ba28b625866e776
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-09-11 09:43:36 +0600

    pushpop

---
+++ push_pop.js
@@ -0,0 +1,10 @@
+var ages = [20, 14, 50, 40, 60];
+// console.log(ages);
+// ages.push(40);
+// console.log(ages);
+// ages.pop();
+console.log(ages);
+ages.pop();
+console.log(ages);
+var elements=ages.pop();
+console.log(elements);
