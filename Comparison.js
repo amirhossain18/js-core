21415b947e9b838293e9f7279c35787ef28f2912
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-09-11 21:02:53 +0600

    if-else

--- Comparison.js
+++ Comparison.js
@@ -1,5 +1,23 @@
-console.log(5==6);
-console.log(5>6);
-console.log(5<6);
-console.log(5 <= 6);
-console.log(5 >= 6);
+// console.log(5==6);
+// console.log(5>6);
+// console.log(5<6);
+// console.log(5 <= 6);
+// console.log(5 >= 6);
+
+var num1 = 169;
+var num2 = 78;
+var num3 = 185;
+// multi condition
+
+if (num1 > num2 && num1 > num3){
+    console.log("number1 is large " , num1);
+
+}
+
+else if (num1 < num2 && num2 > num3){
+    console.log("number2 is Large" , num2);
+}
+
+else{
+    console.log("number3 is large", num3);
+}
