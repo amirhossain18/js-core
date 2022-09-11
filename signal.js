17c79757f2041c57119c7ab3cd864eec0d3708b0
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-09-11 13:11:05 +0600

    condition

---
+++ signal.js
@@ -0,0 +1,17 @@
+var signal=['green', 'red', 'yellow',];
+var state="red"
+
+
+if (state=='green'){
+    console.log("You start walking now");
+}
+else if (state=='red'){
+    console.log("Stop now")
+}
+else if (state=='yellow'){
+    console.log("you wait few moments")
+}
+
+else{
+    console.log("you can walks , no singnal this time")
+}
