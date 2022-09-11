c68c6108d26992b6fa98adde4c0a16649064ac0c
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-09-11 09:34:51 +0600

    change-value

--- aray.js
+++ aray.js
@@ -1,4 +1,16 @@
 var ages = [20, 14, 50, 40, 60];
 var names= ['aman', 'alom', 'amir'];
+var elements =names['2'];
+var positions =names.indexOf('alom');
+console.log(positions);
+console.log(elements);
 console.log(names);
+// array items or length
 console.log(names.length);
+// index ar value
+console.log(names[1]);
+// full array
+console.log(ages);
+// value change
+ages[2]=26;              
+console.log(ages);
